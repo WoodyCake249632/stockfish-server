fe338174797d574b73d5135b60b586ac1c1cf8bb
WoodyCake249632 <damelich423@gmail.com>
2025-06-02 13:21:24 +0200

    main.py

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ def bestmove():
             text=True
         )
         engine.stdin.write(f"position fen {fen}\n")
-        engine.stdin.write("go movetime 1000\n")
+        engine.stdin.write("go movetime 750\n")
         engine.stdin.flush()
 
         while True:
